910b788b96daaa307ed20ab9f0e8c1488b95852e
HarshS490 <harsh.singh30112004@gmail.com>
2025-07-05 18:07:11 +0530

    fix: add check for environment variables

--- app/database/session.py
+++ app/database/session.py
@@ -7,6 +7,8 @@ from sqlalchemy.orm import sessionmaker
 load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
+if DATABASE_URL is None:
+  raise ValueError("DATABASE_URL is not set in the environment")
 
 engine = create_engine(
     DATABASE_URL, connect_args={"check_same_thread": False} if DATABASE_URL.startswith("sqlite") else {}
